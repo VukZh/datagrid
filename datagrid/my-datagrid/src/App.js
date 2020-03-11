effe80d1f37ca0387998c033cf0e829291913cca
VukZh <vukfx@inbox.ru>
2020-03-11 18:40:11 +0600

    feat: implement redux-devtools-extension and Shift event for columns sort

--- datagrid/my-datagrid/src/App.js
+++ datagrid/my-datagrid/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { createStore } from 'redux';
+
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import './App.css';
 import Table from 'react-bootstrap/Table';
+
+const composeEnhancers = composeWithDevTools({
+});
+
+const middlewares = [thunkMiddleware];
 
 const setOrderSym = () => {
   store.dispatch({type: 'SYM_ORDER'});
 }
 const setOrderName = () => {
-//  alert('sss')
   store.dispatch({type: 'NAME_ORDER'});
 }
 const setOrderCap = () => {
@@ -29,86 +37,80 @@ const setOrderMine = () => {
   store.dispatch({type: 'MINE_ORDER'});
 }
 
+
+const setCtrl = () => {
+  store.dispatch({type: 'SET_CTRL'});
+}
+const unsetCtrl = () => {
+  store.dispatch({type: 'UNSET_CTRL'});
+}
+
+
 const initialState = {
-  sym: 'thead__align',
-  name: 'thead__align',
-  cap: 'thead__align',
-  price: 'thead__align',
-  c1h: 'thead__align',
-  c24h: 'thead__align',
-  c1d: 'thead__align',
-  mine: 'thead__align',  
+  order_sym: 'thead__align',
+  order_name: 'thead__align',
+  order_cap: 'thead__align',
+  order_price: 'thead__align',
+  order_c1h: 'thead__align',
+  order_c24h: 'thead__align',
+  order_c1d: 'thead__align',
+  order_mine: 'thead__align',
+  ctrl_btn: false,
 };
 
+const handleKeyDown = (e) => {
+  if (e.key === 'Shift' ) setCtrl();
+}
+const handleKeyUp = (e) => {
+  if (e.key === 'Shift' ) unsetCtrl();
+}
 
-const reducer = (state = initialState, action) => {  
-  switch (action.type){
+
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
+
+const reducer = (state = initialState, action) => {
+  
+  let currState = state.ctrl_btn ? state : initialState;
+  
+  switch (action.type){      
     case 'SYM_ORDER': 
-      if(state.sym === 'thead__align') return Object.assign({}, state, {
-        sym: 'thead__align up'
-      })
-      if(state.sym === 'thead__align up') return Object.assign({}, state, {
-        sym: 'thead__align down'
-      })
-     return Object.assign({}, state, {
-        sym: 'thead__align'
-      });  
+      if(state.order_sym === 'thead__align') return {...currState, order_sym: 'thead__align up'}
+      if(state.order_sym === 'thead__align up') return {...currState, order_sym: 'thead__align down'}
+     return {...currState, order_sym: 'thead__align'};  
     case 'NAME_ORDER': 
-      if(state.name === 'thead__align') return Object.assign({}, state, {
-        name: 'thead__align up'
-      })
-      if(state.name === 'thead__align up') return Object.assign({}, state, {
-        name: 'thead__align down'})
-      return Object.assign({}, state, {
-        name: 'thead__align'});  
+      if(state.order_name === 'thead__align') return {...currState, order_name: 'thead__align up'}
+      if(state.order_name === 'thead__align up') return {...currState, order_name: 'thead__align down'}
+     return {...currState, order_name: 'thead__align'};  
     case 'CAP_ORDER': 
-      if(state.cap === 'thead__align') return Object.assign({}, state, {
-        cap: 'thead__align up'
-      })
-      if(state.cap === 'thead__align up') return Object.assign({}, state, {
-        cap: 'thead__align down'})
-      return Object.assign({}, state, {
-        cap: 'thead__align'});   
+      if(state.order_cap === 'thead__align') return {...currState, order_cap: 'thead__align up'}
+      if(state.order_cap === 'thead__align up') return {...currState, order_cap: 'thead__align down'}
+     return {...currState, order_cap: 'thead__align'};   
     case 'PRICE_ORDER': 
-      if(state.price === 'thead__align') return Object.assign({}, state, {
-        price: 'thead__align up'
-      })
-      if(state.price === 'thead__align up') return Object.assign({}, state, {
-        price: 'thead__align down'})
-      return Object.assign({}, state, {
-        price: 'thead__align'});
+      if(state.order_price === 'thead__align') return {...currState, order_price: 'thead__align up'}
+      if(state.order_price === 'thead__align up') return {...currState, order_price: 'thead__align down'}
+     return {...currState, order_price: 'thead__align'}; 
     case 'C1H_ORDER': 
-      if(state.c1h === 'thead__align') return Object.assign({}, state, {
-        c1h: 'thead__align up'
-      })
-      if(state.c1h === 'thead__align up') return Object.assign({}, state, {
-        c1h: 'thead__align down'})
-      return Object.assign({}, state, {
-        c1h: 'thead__align'}); 
+      if(state.order_c1h === 'thead__align') return {...currState, order_c1h: 'thead__align up'}
+      if(state.order_c1h === 'thead__align up') return {...currState, order_c1h: 'thead__align down'}
+     return {...currState, order_c1h: 'thead__align'}; 
     case 'C24H_ORDER': 
-      if(state.c24h === 'thead__align') return Object.assign({}, state, {
-        c24h: 'thead__align up'
-      })
-      if(state.c24h === 'thead__align up') return Object.assign({}, state, {
-        c24h: 'thead__align down'})
-      return Object.assign({}, state, {
-        c24h: 'thead__align'}); 
+      if(state.order_c24h === 'thead__align') return {...currState, order_c24h: 'thead__align up'}
+      if(state.order_c24h === 'thead__align up') return {...currState, order_c24h: 'thead__align down'}
+     return {...currState, order_c24h: 'thead__align'}; 
     case 'C1D_ORDER': 
-      if(state.c1d === 'thead__align') return Object.assign({}, state, {
-        c1d: 'thead__align up'
-      })
-      if(state.c1d === 'thead__align up') return Object.assign({}, state, {
-        c1d: 'thead__align down'})
-      return Object.assign({}, state, {
-        c1d: 'thead__align'});
+      if(state.order_c1d === 'thead__align') return {...currState, order_c1d: 'thead__align up'}
+      if(state.order_c1d === 'thead__align up') return {...currState, order_c1d: 'thead__align down'}
+     return {...currState, order_c1d: 'thead__align'}; 
     case 'MINE_ORDER': 
-      if(state.mine === 'thead__align') return Object.assign({}, state, {
-        mine: 'thead__align up'
-      })
-      if(state.mine === 'thead__align up') return Object.assign({}, state, {
-        mine: 'thead__align down'})
-      return Object.assign({}, state, {
-        mine: 'thead__align'});
+      if(state.order_mine === 'thead__align') return {...currState, order_mine: 'thead__align up'}
+      if(state.order_mine === 'thead__align up') return {...currState, order_mine: 'thead__align down'}
+     return {...currState, order_mine: 'thead__align'}; 
+      
+    case 'SET_CTRL': 
+      return {...state, ctrl_btn: true};   
+    case 'UNSET_CTRL': 
+      return {...state, ctrl_btn: false};        
       
     default:
       return state;
@@ -117,19 +119,21 @@ const reducer = (state = initialState, action) => {
 
 
 
-const store = createStore(reducer);
 
-store.subscribe(()=>{document.getElementById('sym').className = store.getState().sym})
-store.subscribe(()=>{document.getElementById('name').className = store.getState().name})
-store.subscribe(()=>{document.getElementById('cap').className = store.getState().cap})
-store.subscribe(()=>{document.getElementById('price').className = store.getState().price})
-store.subscribe(()=>{document.getElementById('c1h').className = store.getState().c1h})
-store.subscribe(()=>{document.getElementById('c24h').className = store.getState().c24h})
-store.subscribe(()=>{document.getElementById('c1d').className = store.getState().c1d})
-store.subscribe(()=>{document.getElementById('mine').className = store.getState().mine})
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(...middlewares),
+));
 
-function App() {
-  
+store.subscribe(()=>{document.getElementById('sym').className = store.getState().order_sym})
+store.subscribe(()=>{document.getElementById('name').className = store.getState().order_name})
+store.subscribe(()=>{document.getElementById('cap').className = store.getState().order_cap})
+store.subscribe(()=>{document.getElementById('price').className = store.getState().order_price})
+store.subscribe(()=>{document.getElementById('c1h').className = store.getState().order_c1h})
+store.subscribe(()=>{document.getElementById('c24h').className = store.getState().order_c24h})
+store.subscribe(()=>{document.getElementById('c1d').className = store.getState().order_c1d})
+store.subscribe(()=>{document.getElementById('mine').className = store.getState().order_mine})
+
+function App() { 
 
 
   return (
